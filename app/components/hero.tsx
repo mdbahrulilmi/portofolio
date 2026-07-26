5b3645d09977a91efacd6fbd3a103c3deb89c798
mdbicode <mdbahrulilmi@gmail.com>
2026-07-26 21:40:08 +0700

    feat(hero) make dynamic

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -1,14 +1,19 @@
-export default function Hero(){
+export default function Hero({
+    title, subTitle, buttonTitle
+}:{
+    title:string,
+    subTitle:string,
+    buttonTitle:string,
+}){
     return(
         <div className="hero bg-base-200 min-h-screen">
             <div className="hero-content text-center">
-                <div className="max-w-md">
-                <h1 className="text-5xl font-bold">Hello there</h1>
+                <div className="max-w-2xl">
+                <h1 className="text-5xl font-bold">{ title }</h1>
                 <p className="py-6">
-                    Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                    quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    {subTitle}
                 </p>
-                <button className="btn btn-primary">Get Started</button>
+                <button className="btn btn-primary">{buttonTitle}</button>
                 </div>
             </div>
         </div>
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ import Footer from "./components/footer";
 export default function Home() {
   return (
     <>
-      <Hero/>
+      <Hero
+        title="Membangun Web dari Nol Memang Membingungkan."
+        subTitle="Saya Mdbahrulilmi, seorang Fullstack Developer yang mengubah ide rumit menjadi aplikasi web yang bersih, cepat, dan siap pakai. Jangan biarkan kendala teknis menghentikan visi Anda." 
+        buttonTitle="Lihat Cara Saya Membantu →"
+      />
       <Footer />
     </>
   );
