29776faddeddae89b4bb578c731a4155e36cd87f
mdbicode <mdbahrulilmi@gmail.com>
2026-08-14 13:20:17 +0700

    feat(robots) add robots.ts

---
+++ app/robots.ts
@@ -0,0 +1,11 @@
+import { MetadataRoute } from 'next'
+ 
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://mdbahrulilmi.my.id/sitemap.xml',
+  }
+}
