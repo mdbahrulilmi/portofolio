4b5728f7566665b1db4dd94c4012ca090e9351a3
mdbicode <mdbahrulilmi@gmail.com>
2026-08-20 09:23:38 +0700

    refactor(docs) sorting by date -asc

--- lib/docs.ts
+++ lib/docs.ts
@@ -9,21 +9,24 @@ const docsDirectory = path.join(process.cwd(), 'content/docs');
 export function getAllDocs() {
   const fileNames = fs.readdirSync(docsDirectory);
   
-    return fileNames.map(fileName => {
-    const slug = fileName
-        .replace(/\.md$/, '')
-        .toLowerCase()
-        .replace(/\s+/g, '_');
-        
-    const fileContents = fs.readFileSync(path.join(docsDirectory, fileName), 'utf8');
-    const { data } = matter(fileContents);
-    
-    return {
-        slug,
-        title: data.title || slug,
-        category: data.category || 'Uncategorized',
-    };
+    const allDocs = fileNames.map(fileName => {
+      const slug = fileName
+          .replace(/\.md$/, '')
+          .toLowerCase()
+          .replace(/\s+/g, '_');
+          
+      const fileContents = fs.readFileSync(path.join(docsDirectory, fileName), 'utf8');
+      const { data } = matter(fileContents);
+      
+      return {
+          slug,
+          title: data.title || slug,
+          category: data.category || 'Uncategorized',
+          date: data.date || '1970-01-01',
+      };
     });
+
+    return allDocs.sort((a, b) => a.date.localeCompare(b.date));
 }
 
 export async function getDocBySlug(slug: string) {
